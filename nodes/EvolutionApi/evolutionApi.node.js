f79d6f659588cbb194f39bf1aee7bb0ab24e64a2
OrionDesign <contato@oriondesign.art.br>
2024-07-26 15:57:26 -0300

    Nodes EvolutionAPI

--- nodes/EvolutionApi/evolutionApi.node.js
+++ nodes/EvolutionApi/evolutionApi.node.js
@@ -1,49 +1,81 @@
-import { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import { INodeType, INodeTypeDescription, IExecuteFunctions } from 'n8n-workflow';
 import { httpVerbOperations } from './HttpVerbDescription';
 
-export class evolutionApi implements INodeType {
-    description: INodeTypeDescription = {
-        displayName: 'evolutionApi',
+export class EvolutionApi implements INodeType {
+  description: INodeTypeDescription = {
+    displayName: 'Evolution API',
+    name: 'evolutionApi',
+    icon: 'file:evolutionapi.svg',
+    group: ['transform'],
+    version: 1,
+    subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+    description: 'Interact with Evolution API',
+    defaults: {
+      name: 'Evolution API',
+    },
+    inputs: ['main'],
+    outputs: ['main'],
+    credentials: [
+      {
         name: 'evolutionApi',
-        icon: 'file:evolutionapi.svg',
-        group: ['transform'],
-        version: 1,
-        subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
-        description: 'Interact with EvolutionAPI',
-        defaults: {
-            name: 'evolutionApi',
-        },
-        inputs: ['main'],
-        outputs: ['main'],
-        credentials: [
-            {
-                name: 'evolutionApi',
-                required: true,
-            },
+        required: true,
+      },
+    ],
+    requestDefaults: {
+      baseURL: 'https://evolutionapi.com',
+      url: '',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    },
+    properties: [
+      {
+        displayName: 'Resource',
+        name: 'esource',
+        type: 'options',
+        noDataExpression: true,
+        options: [
+          {
+            name: 'HTTP Verb',
+            value: 'httpVerb',
+          },
         ],
-        requestDefaults: {
-            baseURL: 'https://evolutionapi.com',
-            url: '',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-        },
-        properties: [
-            {
-                displayName: 'Resource',
-                name: 'resource',
-                type: 'options',
-                noDataExpression: true,
-                options: [
-                    {
-                        name: 'HTTP Verb',
-                        value: 'httpVerb',
-                    },
-                ],
-                default: 'httpVerb',
-            },
-            ...httpVerbOperations,
-        ],
-    };
+        default: 'httpVerb',
+      },
+     ...httpVerbOperations,
+    ],
+  };
+
+  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    const items = this.getInputData();
+    const returnData: INodeExecutionData[][] = [];
+
+    for (let i = 0; i < items.length; i++) {
+      const resource = this.getNodeParameter('resource', i) as string;
+      const operation = this.getNodeParameter('operation', i) as string;
+      const headers = this.getNodeParameter('headers', i) as { [key: string]: string };
+      const body = this.getNodeParameter('body', i) as string;
+
+      const options = {
+        method: operation,
+        uri: `https://evolutionapi.com/${resource}`,
+        headers,
+        body,
+      };
+
+      try {
+        const response = await this.helpers.httpRequest(options);
+        returnData.push([this.helpers.returnJsonArray(response)]);
+      } catch (error) {
+        throw new Error(`Error executing Evolution API request: ${error.message}`);
+      }
+    }
+
+    return returnData;
+  }
 }
+
+export default () => {
+  return new EvolutionApi();
+};
