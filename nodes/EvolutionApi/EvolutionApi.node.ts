ca763f05099577a5adf7014d217c3677e3f26152
OrionDesign <contato@oriondesign.art.br>
2024-07-26 14:29:07 -0300

    Update EvolutionApi.node.ts

--- nodes/EvolutionApi/EvolutionApi.node.ts
+++ nodes/EvolutionApi/EvolutionApi.node.ts
@@ -2,49 +2,49 @@ import { INodeType, INodeTypeDescription } from 'n8n-workflow';
 import { httpVerbFields, httpVerbOperations } from './HttpVerbDescription';
 
 export class EvolutionApi implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'EvolutionAPI',
-		name: 'evolutionApi', // Changed to match the name in the JSON
-		icon: 'file:evolutionapi.svg',
-		group: ['transform'],
-		version: 1,
-		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
-		description: 'Interact with EvolutionAPI',
-		defaults: {
-			name: 'EvolutionAPI',
-		},
-		inputs: ['main'],
-		outputs: ['main'],
-		credentials: [
-			{
-				name: 'evolutionApi', // Changed to match the name in the credentials class
-				required: true, // or false, depending on if it's required
-			},
-		],
-		requestDefaults: {
-			baseURL: 'https://evolutionapi.com',
-			url: '',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		},
-		properties: [
-			{
-				displayName: 'Resource',
-				name: 'resource',
-				type: 'options',
-				noDataExpression: true,
-				options: [
-					{
-						name: 'HTTP Verb',
-						value: 'httpVerb',
-					},
-				],
-				default: 'httpVerb',
-			},
-			...httpVerbOperations,
-			...httpVerbFields,
-		],
-	};
+    description: INodeTypeDescription = {
+        displayName: 'EvolutionAPI',
+        name: 'evolutionApi',
+        icon: 'file:evolutionapi.svg',
+        group: ['transform'],
+        version: 1,
+        subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+        description: 'Interact with EvolutionAPI',
+        defaults: {
+            name: 'EvolutionAPI',
+        },
+        inputs: ['main'],
+        outputs: ['main'],
+        credentials: [
+            {
+                name: 'evolutionApi',
+                required: true,
+            },
+        ],
+        requestDefaults: {
+            baseURL: 'https://evolutionapi.com',
+            url: '',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        },
+        properties: [
+            {
+                displayName: 'Resource',
+                name: 'resource',
+                type: 'options',
+                noDataExpression: true,
+                options: [
+                    {
+                        name: 'HTTP Verb',
+                        value: 'httpVerb',
+                    },
+                ],
+                default: 'httpVerb',
+            },
+            ...httpVerbOperations,
+            ...httpVerbFields,
+        ],
+    };
 }
